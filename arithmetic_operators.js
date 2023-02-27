b8cf27f2a41ab51772f2a007872c0b0b99b30579
ajlakorac <119134336+ajlakorac@users.noreply.github.com>
2023-02-27 22:35:47 +0100

    Add files via upload

---
+++ arithmetic_operators.js
@@ -0,0 +1,21 @@
+let number1 =3;
+let number2 = 10;
+let sum = number1 + number2;
+let poruka = "Suma je" + sum;
+console.log(poruka);
+
+let razlika = number1 - number2;
+poruka = "Razlika je" + razlika;
+console.log(poruka);
+
+let product = number1 * number2;
+poruka = "Umnozak je" + product;
+console.log(poruka);
+
+let division = number1 / number2;
+poruka = "Rezultat dijeljenja je" + division;
+console.log(poruka);
+
+
+
+
